93ba2ea7dc38b59a2c05477cde97713ccc52bdd3
JamesCordova <jcordovas@unsa.edu.pe>
2025-05-03 23:07:15 -0500

    Class component and use on main:\n\nWe created another class on CTK to make the mid fram and have two textoboxes, one entry and two buttons

--- app/gui/components.py
+++ app/gui/components.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import app.config as cf
 
 class HeaderFrame(ctk.CTkFrame):
     def __init__(self, master=None, list_choices=None, selected:str=None, **kwargs):
@@ -18,3 +19,61 @@ class HeaderFrame(ctk.CTkFrame):
         print(f"Combobox changed to: {choice}")
         self.selected[0] = choice
         self.title_label.configure(text=choice)
+
+class InputOutputFrame(ctk.CTkFrame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        # First the 3 frames with grid layout
+        # the input side of the mid frame
+        self.input_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.input_side.grid(row=0, column=0, sticky="nsew")
+        
+        # the frame of buttons and KEY entry box
+        self.operation_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.operation_side.grid(row=0, column=1, sticky="nsew")
+        
+        # the output side of the mid frame
+        self.output_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.output_side.grid(row=0, column=2, sticky="nsew")
+        
+        
+        # Input side: the label and the textbox for the input text
+        self.input_label = ctk.CTkLabel(self.input_side, text="Insert the input text", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.input_label.pack(side="top", fill="x")
+        self.input_textbox = ctk.CTkTextbox(self.input_side, wrap="word", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.input_textbox.pack(side="top", fill="both", expand=True)
+        
+        
+        # Operation side: the frame will have a button and a entry box for the key
+        self.key_entry = ctk.CTkEntry(self.operation_side, placeholder_text="Key", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.key_entry.pack(side="top", fill="x", anchor="center", expand=True)
+        # two buttons for the cypher and decypher
+        self.cypher_button = ctk.CTkButton(self.operation_side, text="Cypher", corner_radius = 15,
+                fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
+                hover_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK), command=self.cypher_content)
+        self.cypher_button.pack(side="top", fill="x", anchor="center", expand=True)
+        self.decypher_button = ctk.CTkButton(self.operation_side, text="Decypher", corner_radius = 15,
+                fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
+                hover_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK), command=self.decypher_content)
+        self.decypher_button.pack(side="top", fill="x", anchor="center", expand=True)
+        
+    
+        # Output side: the label and the textbox for the input text
+        self.output_label = ctk.CTkLabel(self.output_side, text="Insert the output text", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.output_label.pack(side="top", fill="x")
+        self.output_textbox = ctk.CTkTextbox(self.output_side, wrap="word", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.output_textbox.pack(side="top", fill="both", expand=True)
+        
+        # Configure the grid layout
+        self.grid_columnconfigure(0, weight=5)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=5)
+        self.grid_rowconfigure(0, weight=1)
+        
+    def cypher_content(self):
+        print("Cypher button clicked")
+        pass
+    
+    def decypher_content(self):
+        print("Decypher button clicked")
+        pass
--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -19,15 +19,13 @@ class MainWindow(ctk.CTk):
         # Frame with header and combobox
         self.title_frame = components.HeaderFrame(self, list_choices=self.algorithms, selected=self.algorithm_selected, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
         self.title_frame.place(relx=0, rely=0, relwidth=1, relheight=0.1, anchor="nw")
-        self.input_output_frame = ctk.CTkFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.input_output_frame = components.InputOutputFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
         self.input_output_frame.place(relx=0, rely=0.1, relwidth=1, relheight=0.8, anchor="nw")
         self.alphabet_frame = ctk.CTkFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
         self.alphabet_frame.place(relx=0, rely=0.9, relwidth=1, relheight=0.1, anchor="nw")
         
-        # Setting the title frame
-        self.button = ctk.CTkButton(self.input_output_frame, text="Click Me", command=self.on_button_click)
-        self.button.place(relx=0.5, rely=0.5, anchor="center")
-    
+        
+
         
     def on_button_click(self):
         print("Selected algorithm:", self.algorithm_selected)
