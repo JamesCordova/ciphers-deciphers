b07e234511b8a0e95e9cee3d0d8acc7624a427c6
JamesCordova <jcordovas@unsa.edu.pe>
2025-05-04 00:29:31 -0500

    Add tuning to buttons, texts and paddings\n\nWe are setting up the padding and all of thtat to make more fancy

--- app/gui/components.py
+++ app/gui/components.py
@@ -2,10 +2,11 @@ import customtkinter as ctk
 import app.config as cf
 
 class HeaderFrame(ctk.CTkFrame):
-    def __init__(self, master=None, list_choices=None, selected:str=None, **kwargs):
+    def __init__(self, master=None, list_choices=["No encontrado"], selected_ref=["No encontrado"], **kwargs):
         super().__init__(master, **kwargs)
+        # Variables
         self.list = list_choices
-        self.selected = selected
+        self.selected = selected_ref
         # Create a title_label
         self.title_label = ctk.CTkLabel(self, text=self.selected[0], wraplength=650, anchor="w", font=("Arial", 25))
         self.title_label.place(relx=0.02, rely=0.5, relwidth=0.75, anchor="w")
@@ -31,44 +32,54 @@ class InputOutputFrame(ctk.CTkFrame):
         super().__init__(master, **kwargs)
         # First the 3 frames with grid layout
         # the input side of the mid frame
-        self.input_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.input_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.input_side.grid(row=0, column=0, sticky="nsew")
         
         # the frame of buttons and KEY entry box
-        self.operation_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
-        self.operation_side.grid(row=0, column=1, sticky="nsew")
+        self.operation_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.operation_side.grid(row=0, column=1, sticky="ew")
         
         # the output side of the mid frame
-        self.output_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.output_side = ctk.CTkFrame(self, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.output_side.grid(row=0, column=2, sticky="nsew")
         
         
         # Input side: the label and the textbox for the input text
         self.input_label = ctk.CTkLabel(self.input_side, text="Insert the input text", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.input_label.pack(side="top", fill="x")
-        self.input_textbox = ctk.CTkTextbox(self.input_side, wrap="word", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
-        self.input_textbox.pack(side="top", fill="both", expand=True)
+        self.input_textbox = ctk.CTkTextbox(self.input_side, 
+                                            wrap="word", 
+                                            corner_radius=20,
+                                            fg_color=(cf.ACCENT_COLOR_LIGHT, cf.ACCENT_COLOR_DARK))
+        self.input_textbox.pack(side="top", fill="both", expand=True, padx=20, pady=10)
         
         
         # Operation side: the frame will have a button and a entry box for the key
-        self.key_entry = ctk.CTkEntry(self.operation_side, placeholder_text="Key", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
-        self.key_entry.pack(side="top", fill="x", anchor="center", expand=True)
+        self.key_entry = ctk.CTkEntry(self.operation_side,placeholder_text="Clave", corner_radius=20, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.key_entry.pack(side="top", fill="x", anchor="center", expand=True, padx=15, pady=10)
         # two buttons for the cypher and decypher
-        self.cypher_button = ctk.CTkButton(self.operation_side, text="Cypher", corner_radius = 15,
-                fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
-                hover_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK), command=self.cypher_content)
-        self.cypher_button.pack(side="top", fill="x", anchor="center", expand=True)
-        self.decypher_button = ctk.CTkButton(self.operation_side, text="Decypher", corner_radius = 15,
-                fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
-                hover_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK), command=self.decypher_content)
-        self.decypher_button.pack(side="top", fill="x", anchor="center", expand=True)
+        self.cypher_button = ctk.CTkButton(self.operation_side, 
+                                           text="Cypher >", 
+                                           corner_radius = 15,
+                                            fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
+                                            hover_color = (cf.SUCCESS_COLOR_LIGHT, cf.SUCCESS_COLOR_DARK), command=self.cypher_content)
+        self.cypher_button.pack(side="top", fill="x", anchor="center", expand=True, padx=15, pady=10)
+        self.decypher_button = ctk.CTkButton(self.operation_side, 
+                                             text="< Decypher", 
+                                             corner_radius = 15,
+                                             fg_color = (cf.MESSAGE_COLOR_LIGHT, cf.MESSAGE_COLOR_DARK),
+                                             hover_color = (cf.SUCCESS_COLOR_LIGHT, cf.SUCCESS_COLOR_DARK), command=self.decypher_content)
+        self.decypher_button.pack(side="top", fill="x", anchor="center", expand=True, padx=15, pady=10)
         
     
         # Output side: the label and the textbox for the input text
         self.output_label = ctk.CTkLabel(self.output_side, text="Insert the output text", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.output_label.pack(side="top", fill="x")
-        self.output_textbox = ctk.CTkTextbox(self.output_side, wrap="word", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
-        self.output_textbox.pack(side="top", fill="both", expand=True)
+        self.output_textbox = ctk.CTkTextbox(self.output_side, 
+                                            wrap="word", 
+                                            corner_radius=20,
+                                            fg_color=(cf.ACCENT_COLOR_LIGHT, cf.ACCENT_COLOR_DARK))
+        self.output_textbox.pack(side="top", fill="both", expand=True, padx=20, pady=10)
         
         # Configure the grid layout
         self.grid_columnconfigure(0, weight=5)
@@ -85,24 +96,29 @@ class InputOutputFrame(ctk.CTkFrame):
         pass
     
 class AlphabetFrame(ctk.CTkFrame):
-    def __init__(self, master=None, **kwargs):
+    def __init__(self, master=None, list_choices=["Español"], selected_ref=["Español"], **kwargs):
         super().__init__(master, **kwargs)
+        # Variables
+        self.list = list_choices
+        self.selected = selected_ref
         # Create a label for the alphabet
-        self.alphabet_label = ctk.CTkLabel(self, text="Alphabet", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
-        self.alphabet_label.pack(side="top", fill="x")
+        self.alphabet_label = ctk.CTkLabel(self, text="Alphabet", anchor="center", fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.alphabet_label.pack(side="top", fill="x", padx=20, pady=2)
         # Create a segmented button for the alphabet options
         self.alphabet_options = ctk.CTkSegmentedButton(self, 
-                                                       values=["Option 1", "Option 2", "Option 3"], 
+                                                       values=self.list, 
                                                        corner_radius=20,
                                                        bg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK),
                                                        fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK),
                                                        selected_color=(cf.SUCCESS_COLOR_LIGHT, cf.SUCCESS_COLOR_DARK),
                                                        selected_hover_color=(cf.SUCCESS_COLOR_LIGHT, cf.SUCCESS_COLOR_DARK),
                                                        command=self.on_segmented_button_change)
-        self.alphabet_options.pack(side="top", fill="x")
+        self.alphabet_options.set(self.selected[0])
+        self.alphabet_options.pack(side="top", fill="x", padx=20, pady=2)
         # Create a label for the current alphabet
-        self.current_alphabet_entry = ctk.CTkEntry(self, placeholder_text="Current Alphabet", corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
-        self.current_alphabet_entry.pack(side="top", fill="x", anchor="n", expand=True)
+        self.current_alphabet_entry = ctk.CTkEntry(self, placeholder_text="Current Alphabet", corner_radius=20, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.current_alphabet_entry.insert(0, self.selected[0])
+        self.current_alphabet_entry.pack(side="top", fill="x", anchor="n", expand=True, padx=20, pady=2)
         
     def on_segmented_button_change(self, value):
         print("Segmented button changed to:", value)
--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -11,7 +11,7 @@ class MainWindow(ctk.CTk):
         self.color_ui = (cf.BG_COLOR_DARK, cf.BG_COLOR_LIGHT)
         self.configure(background = self.color_ui)
         
-        # Not Widegets variables
+        # Not Widgets variables
         self.algorithms = ["Transformación Columnar Simple",
                            "Transformación Columnar Doble",
                            "Rejillas criptográficas",
@@ -19,13 +19,20 @@ class MainWindow(ctk.CTk):
                            "Permutación por series",
                            "Playfair"]
         self.algorithm_selected = [self.algorithms[0]]
+        self.alphabets = ["Español",
+                          "Inglés",
+                          "Francés",
+                          "Alemán",
+                          "Italiano",
+                          "Portugués"]
+        self.alphabet_selected = [self.alphabets[0]]
         
         # Create 3 frames with grid layout
         # Frame with header and combobox
-        self.title_frame = components.HeaderFrame(self, list_choices=self.algorithms, selected=self.algorithm_selected, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
+        self.title_frame = components.HeaderFrame(self, list_choices=self.algorithms, selected_ref=self.algorithm_selected, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.title_frame.place(relx=0, rely=0, relwidth=1, relheight=0.1, anchor="nw")
-        self.input_output_frame = components.InputOutputFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.input_output_frame = components.InputOutputFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.input_output_frame.place(relx=0, rely=0.1, relwidth=1, relheight=0.7, anchor="nw")
-        self.alphabet_frame = components.AlphabetFrame(self, corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK), border_color=cf.ERROR_COLOR_DARK, border_width=1)
+        self.alphabet_frame = components.AlphabetFrame(self, list_choices=self.alphabets, selected_ref=self.alphabet_selected,corner_radius=0, fg_color=(cf.BG_COLOR_LIGHT, cf.BG_COLOR_DARK))
         self.alphabet_frame.place(relx=0, rely=0.8, relwidth=1, relheight=0.2, anchor="nw") 
 
